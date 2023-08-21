14bee3807e081d1224e4eadfb2ac48fdd5814328
Sulakna Karunaratna <sulakna.karunaratna@arimaclanka.com>
2023-08-21 01:05:46 -0400

    Adding practice slides

--- countapp/src/data/practiceData.js
+++ countapp/src/data/practiceData.js
@@ -129,10 +129,341 @@ const practiceData = {
       height: "200px"
     },
     {
-      id: 2,
+      id: 3,
       img: require("../assests/animals/cat.png"),
       top: "50%",
       left: "55%",
+      height: "250px"
+    },
+  ],
+}, {
+  animals: [
+    {
+      id: 1,
+      img: require("../assests/animals/giraffe.png"),
+      top: "15%",
+      left: "10%",
+      height: "150px"
+    },
+    {
+      id: 2,
+      img: require("../assests/animals/giraffe.png"),
+      top: "50%",
+      left: "10%",
+      height: "220px"
+    },
+    {
+      id: 3,
+      img: require("../assests/animals/giraffe.png"),
+      top: "15%",
+      left: "35%",
+      height: "250px"
+    },    
+    {
+      id: 4,
+      img: require("../assests/animals/giraffe.png"),
+      top: "65%",
+      left: "35%",
+      height: "150px"
+    },
+    {
+      id: 5,
+      img: require("../assests/animals/giraffe.png"),
+      top: "15%",
+      left: "55%",
+      height: "150px"
+    },
+    {
+      id: 6,
+      img: require("../assests/animals/giraffe.png"),
+      top: "65%",
+      left: "55%",
+      height: "200px"
+    },
+    {
+      id: 7,
+      img: require("../assests/animals/giraffe.png"),
+      top: "40%",
+      left: "75%",
+      height: "190px"
+    },
+  ],
+},{
+  animals: [
+    {
+      id: 1,
+      img: require("../assests/animals/dog.png"),
+      top: "15%",
+      left: "25%",
+      height: "200px"
+    },
+    {
+      id: 2,
+      img: require("../assests/animals/dog.png"),
+      top: "50%",
+      left: "25%",
+      height: "170px"
+    },
+    {
+      id: 3,
+      img: require("../assests/animals/dog.png"),
+      top: "20%",
+      left: "55%",
+      height: "150px"
+    },
+    {
+      id: 4,
+      img: require("../assests/animals/dog.png"),
+      top: "50%",
+      left: "55%",
+      height: "250px"
+    },
+  ],
+},
+{
+  animals: [
+    {
+      id: 1,
+      img: require("../assests/animals/duck.png"),
+      top: "25%",
+      left: "30%",
+      height: "350px"
+    },
+  ],
+},
+{
+  animals: [
+    {
+      id: 1,
+      img: require("../assests/animals/lion.png"),
+      top: "25%",
+      left: "50%",
+      height: "200px"
+    },
+    {
+      id: 2,
+      img: require("../assests/animals/lion.png"),
+      top: "50%",
+      left: "30%",
+      height: "250px"
+    },
+  ],
+},{
+  animals: [
+    {
+      id: 1,
+      img: require("../assests/animals/monkey.png"),
+      top: "20%",
+      left: "20%",
+      height: "400px"
+    },
+    {
+      id: 2,
+      img: require("../assests/animals/monkey.png"),
+      top: "15%",
+      left: "60%",
+      height: "200px"
+    },
+    {
+      id: 3,
+      img: require("../assests/animals/monkey.png"),
+      top: "65%",
+      left: "55%",
+      height: "170px"
+    },
+  ],
+},{
+  animals: [
+    {
+      id: 1,
+      img: require("../assests/animals/bird.png"),
+      top: "15%",
+      left: "30%",
+      height: "220px"
+    },
+    {
+      id: 2,
+      img: require("../assests/animals/bird.png"),
+      top: "50%",
+      left: "25%",
+      height: "170px"
+    },
+    {
+      id: 3,
+      img: require("../assests/animals/bird.png"),
+      top: "20%",
+      left: "55%",
+      height: "220px"
+    },
+    {
+      id: 4,
+      img: require("../assests/animals/bird.png"),
+      top: "50%",
+      left: "55%",
+      height: "250px"
+    },
+  ],
+},{
+  animals: [
+    {
+      id: 1,
+      img: require("../assests/animals/owl.png"),
+      top: "25%",
+      left: "40%",
+      height: "300px"
+    },
+  ],
+},{
+  animals: [
+    {
+      id: 1,
+      img: require("../assests/animals/penguin.png"),
+      top: "25%",
+      left: "55%",
+      height: "200px"
+    },
+    {
+      id: 2,
+      img: require("../assests/animals/penguin.png"),
+      top: "50%",
+      left: "30%",
+      height: "250px"
+    },
+  ],
+},{
+  animals: [
+    {
+      id: 1,
+      img: require("../assests/animals/cow.png"),
+      top: "20%",
+      left: "16%",
+      height: "320px"
+    },
+    {
+      id: 2,
+      img: require("../assests/animals/cow.png"),
+      top: "15%",
+      left: "60%",
+      height: "170px"
+    },
+    {
+      id: 3,
+      img: require("../assests/animals/cow.png"),
+      top: "65%",
+      left: "55%",
+      height: "200px"
+    },
+  ],
+}, {
+  animals: [
+    {
+      id: 1,
+      img: require("../assests/animals/raccoon.png"),
+      top: "15%",
+      left: "35%",
+      height: "170px"
+    },
+    {
+      id: 2,
+      img: require("../assests/animals/raccoon.png"),
+      top: "50%",
+      left: "25%",
+      height: "250px"
+    },
+    {
+      id: 3,
+      img: require("../assests/animals/raccoon.png"),
+      top: "20%",
+      left: "65%",
+      height: "150px"
+    },
+    {
+      id: 4,
+      img: require("../assests/animals/raccoon.png"),
+      top: "50%",
+      left: "55%",
+      height: "250px"
+    },
+  ],
+},{
+  animals: [
+    {
+      id: 1,
+      img: require("../assests/animals/sheep.png"),
+      top: "25%",
+      left: "40%",
+      height: "300px"
+    },
+  ],
+},{
+  animals: [
+    {
+      id: 1,
+      img: require("../assests/animals/whale.png"),
+      top: "25%",
+      left: "25%",
+      height: "250px"
+    },
+    {
+      id: 2,
+      img: require("../assests/animals/whale.png"),
+      top: "40%",
+      left: "50%",
+      height: "250px"
+    },
+  ],
+},{
+  animals: [
+    {
+      id: 1,
+      img: require("../assests/animals/otter.png"),
+      top: "20%",
+      left: "16%",
+      height: "260px"
+    },
+    {
+      id: 2,
+      img: require("../assests/animals/otter.png"),
+      top: "50%",
+      left: "38%",
+      height: "280px"
+    },
+    {
+      id: 3,
+      img: require("../assests/animals/otter.png"),
+      top: "55%",
+      left: "60%",
+      height: "220px"
+    },
+  ],
+},{
+  animals: [
+    {
+      id: 1,
+      img: require("../assests/animals/chicken.png"),
+      top: "15%",
+      left: "35%",
+      height: "170px"
+    },
+    {
+      id: 2,
+      img: require("../assests/animals/chicken.png"),
+      top: "50%",
+      left: "15%",
+      height: "250px"
+    },
+    {
+      id: 3,
+      img: require("../assests/animals/chicken.png"),
+      top: "20%",
+      left: "65%",
+      height: "250px"
+    },
+    {
+      id: 4,
+      img: require("../assests/animals/chicken.png"),
+      top: "50%",
+      left: "45%",
       height: "250px"
     },
   ],
